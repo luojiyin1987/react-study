17ca539226311ec1c2a75cafbf816c5f34e6c105
luojiyin <luojiyin@hotmail.com>
2022-01-29 16:42:27 +0800

    dialog 添加到首页

--- example.tsx
+++ example.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter as Router,Route,  Link} from 'react-router-dom';
 import IconExample from './lib/icon/icon.example';
+import DialogExample from './lib/dialog/dialog.example';
 
 
 ReactDOM.render(
@@ -20,11 +21,15 @@ ReactDOM.render(
             <li>
               <Link to="/icon">Icon</Link>
             </li>
+            <li>
+              <Link to="dialog">Dialog</Link>
+            </li>
           
           </ul>
         </aside>
         <main>
         <Route path="/icon" component={IconExample}/>
+        <Route path="/dialog" component={DialogExample}/>
         </main>
       </div>
     </div>
